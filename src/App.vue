22279dc869675b4dd781f7d30dae2d395770850b
zhj <1069178752@qq.com>
2023-05-23 10:45:25 +0800

    feat: 添加配置文件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
+  <div class="box">
     <h1>我是APP</h1>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
-</style> 
+<style scoped lang="scss">
+.box {
+  width: 100px;
+  height: 100px;
+  color: red;
+}
+</style>
